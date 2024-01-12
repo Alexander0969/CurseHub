5ccaa1169ff6a953015d4ee2f508db45c40cbf9b
AliiXXer <138340159+Alexander0969@users.noreply.github.com>
2024-01-11 18:45:04 -0800

    Create [NOT BANNABLE] ESP.lua

---
+++ [NOT BANNABLE] ESP.lua
@@ -0,0 +1,5 @@
+while true do
+  wait(1)
+  print("CurseHub")
+loadstring(gameHttpGet(('https://raw.githubusercontent.com/Alexander0969/CurseHub/main/%5Bdont%20use%20it%20is%20bannable%5DESP.lua')))()
+end
