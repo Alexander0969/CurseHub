9dc99bcd0a79bd0f76bba2df45730a406d64d277
AliiXXer <138340159+Alexander0969@users.noreply.github.com>
2024-01-11 18:38:38 -0800

    Update and rename CurseHubPlayerHighlight.lua to [dont use it is bannable]ESP.lua

--- CurseHubPlayerHighlight.lua
+++
@@ -1,6 +0,0 @@
--- [wip currently]
-local Players = game:GetService("Players")
-
-local function highlightPlayer(player)
-  local highlight = Instance.new("Highlight")
-  highlihgt.Parent = player.Character
